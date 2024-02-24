28c47b1bd865de85ae90b02814dd366304cfe47f
sandy <sandy.ali.basha@gmail.com>
2024-02-24 20:04:35 +0300

    update

--- js/textAnimation.js
+++ js/textAnimation.js
@@ -1,36 +1,37 @@
 // Add will-change property to the elements you're going to animate
 document.querySelector('.imgToTop').style.willChange = 'transform';
 document.querySelector('.imgToBottom').style.willChange = 'transform';
+if (window.innerWidth > 768) { // Check if the screen width is greater than 768px
+    let imgToTop = gsap.timeline({
+        scrollTrigger: {
+            trigger: ".imgToTop",
+            id: "imgToTop",
+            scrub: 2,
+            start: "center center",
+            end: "top top",
+            toggleActions: "resume pause resume pause",
+        },
+    });
+    imgToTop.to(".imgToTop", {
+        y: "-8vh",
+        duration: 10,
+        ease: "power1.in"
+    })
 
-let imgToTop = gsap.timeline({
-    scrollTrigger: {
-        trigger: ".imgToTop",
-        id: "imgToTop",
-        scrub: 2,
-        start: "center center",
-        end: "top top",
-        toggleActions: "resume pause resume pause",
-    },
-});
-imgToTop.to(".imgToTop", {
-    y: "-8vh",
-    duration: 10,
-    ease: "power1.in"
-})
-
-// imgToBottom
-let imgToBottom = gsap.timeline({
-    scrollTrigger: {
-        trigger: ".imgToBottom",
-        id: "imgToBottom",
-        scrub: 2,
-        start: "center center",
-        end: "top top",
-        toggleActions: "resume pause resume pause",
-    },
-});
-imgToBottom.to(".imgToBottom", {
-    y: "10vh",
-    duration: 10,
-    ease: "power1.in"
-})
+    // imgToBottom
+    let imgToBottom = gsap.timeline({
+        scrollTrigger: {
+            trigger: ".imgToBottom",
+            id: "imgToBottom",
+            scrub: 2,
+            start: "center center",
+            end: "top top",
+            toggleActions: "resume pause resume pause",
+        },
+    });
+    imgToBottom.to(".imgToBottom", {
+        y: "10vh",
+        duration: 10,
+        ease: "power1.in"
+    })
+}
